146eb9a7b4b05a8a5bc40a44e0d9174f2558fb81
sophieleegoh <sophieleegoh@gmail.com>
2025-09-21 22:53:29 +1000

    update logo icon

--- ui/src/pages/_document.tsx
+++ ui/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/webp" href="/favicon/two-rookie-cooks-logo.webp" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
