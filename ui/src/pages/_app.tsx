fd6dd57c501b815fcc8b3aa0e3cdf61cb84d07a5
sophieleegoh <sophieleegoh@gmail.com>
2025-09-24 16:48:58 +1000

    Add gtag

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,11 +1,50 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecipesProvider } from "@/context/RecipesContext";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (window.gtag) {
+        window.gtag("event", "page_view", { page_path: url });
+      }
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <RecipesProvider initialRecipes={pageProps.recipes}>
-      <Component {...pageProps} />
-    </RecipesProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-D9SBV9GRSC"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-D9SBV9GRSC', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <RecipesProvider initialRecipes={pageProps.recipes}>
+        <Component {...pageProps} />
+      </RecipesProvider>
+    </>
   );
 }
